a7741934d0b3c1f9e787cc03f5166ca7a574327d
Randy Chrisp <r.chrisp@outlook.com>
2021-11-07 12:36:16 -0500

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using TicTacToe.Models;
+
+namespace TicTacToe.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult squareClicked()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/Game.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TicTacToe.Models
+{
+    public class Game
+    {
+        public Player X { get; set; }
+        public Player O { get; set; }
+        public string CurrentPlayer { get; set; }
+
+        public bool IsGameOver = false;
+    }
+}
